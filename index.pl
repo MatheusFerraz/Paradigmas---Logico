bb322c113835051b92100213b1f9ce5753d57305
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2014-05-18 17:44:14 -0300

    Listas Prontas

--- index.pl
+++ index.pl
@@ -7,6 +7,8 @@
 % IfThenElse
 ifThenElse(X,Y,_):- X,!,Y.
 ifThenElse(_,_,Z):- Z.
+% ListSplit
+listSplit([H|T], H, T).
 
 % Chamada de Inicio
 main():-
@@ -28,8 +30,79 @@ canvas():-
 	% Mostrando o Resultado, ao clicar em um membro da Lista e no botao, o texto aparece no campo.
 	wcreate((main,10),listbox,``,10,220,100,200,16'50a10002),
 	wcreate((main,11),static,``,130,210,400,200,16'50800001),
-	wcreate((main,12),button,`Confirmar`,30,410,60,20,16'50010000).
+	wcreate((main,12),button,`Confirmar`,30,410,60,20,16'50010000),
+	mainloop().
+	
+mainloop():-
+	campoA().
+	
+reset():-
+	wbtnsel((main,1),0),
+	wbtnsel((main,2),0),
+	wbtnsel((main,3),0),
+	wbtnsel((main,4),0),
+	wbtnsel((main,5),0),
+	mainloop().
 
+campoA():-
+	wbtnsel((main,1),S),
+	ifThenElse(S=:=1,listaNome(),campoB()).
 
+campoB():-
+	wbtnsel((main,2),S),
+	ifThenElse(S=:=1,listaTrans(),campoC()).
+
+campoC():-
+	wbtnsel((main,3),S),
+	ifThenElse(S=:=1,listaCausa(),campoD()).
+
+campoD():-
+	wbtnsel((main,4),S),
+	ifThenElse(S=:=1,listaSint(),campoE()).
+
+campoE():-
+	wbtnsel((main,5),S),
+	ifThenElse(S=:=1,listaProf(),mainloop()).	
+
+listaNome():-
+	wcreate((main,10),listbox,``,10,220,100,200,16'50a10002),
+	wcreate((main,11),static,``,130,210,400,200,16'50800001),
+	setof(X,par(doenca,X),L),
+	compoeLista(L).
+
+listaTrans():-
+	wcreate((main,10),listbox,``,10,220,300,200,16'50a10002),
+	wcreate((main,11),static,``,330,210,280,200,16'50800001),
+	setof(X,par(formaTransmissao,X),L),
+	compoeLista(L).
+	
+listaCausa():-
+	wcreate((main,10),listbox,``,10,220,100,200,16'50a10002),
+	wcreate((main,11),static,``,130,210,400,200,16'50800001),
+	setof(X,par(agenteCausador,X),L),
+	compoeLista(L).
+
+listaSint():-
+	wcreate((main,10),listbox,``,10,220,200,200,16'50a10002),
+	wcreate((main,11),static,``,230,210,380,200,16'50800001),
+	setof(X,par(sintoma,X),L),
+	compoeLista(L).
+
+listaProf():-
+	wcreate((main,10),listbox,``,10,220,200,200,16'50a10002),
+	wcreate((main,11),static,``,230,210,380,200,16'50800001),
+	setof(X,par(medidasProfilaticas,X),L),
+	compoeLista(L).
+	
+compoeLista(T):-
+	listSplit(T,A,B),
+	wlbxadd((main,10),0,A),
+	ifThenElse(B==[],reset(),compoeRLista(B)).
+
+compoeRLista(K):-
+	listSplit(K,A,B),
+	wlbxadd((main,10),-1,A),
+	ifThenElse(B==[],reset(),compoeRLista(B)).
+	
 helpcomandos():-
-wcreate(help,text,`BCD - Guia de Comandos`,0,0,640,480,0).
+	wcreate(help,text,`BCD - Guia de Comandos`,0,0,640,480,0).
--- listas.pl
+++ listas.pl
@@ -5,3 +5,26 @@ par(doenca,`Febre Amarela`).
 par(doenca,`Raiva`).
 par(doenca,`Gripe`).
 par(doenca,`Catapora`).
+
+par(formaTransmissao,`Saliva`).
+par(formaTransmissao,`Contato com Objeto Contaminado`).
+par(formaTransmissao,`Picada`).
+par(formaTransmissao,`Mordedura de Animal Infectado`).
+
+par(agenteCausador,`Virus`).
+
+par(sintoma,`Inflamacao das Parotidas`).
+par(sintoma,`Aspecto Amarelado da Pele`).
+par(sintoma,`Danos ao Figado`).
+par(sintoma,`Alteracoes Respiratorias`).
+par(sintoma,`Aumento da Frequencia Cardiaca`).
+par(sintoma,`Danos ao Sistema Nervoso Central`).
+par(sintoma,`Coriza`).
+par(sintoma,`Tosse`).
+par(sintoma,`Dores Musculares`).
+par(sintoma,`Numerosas Feridas pelo Corpo`).
+
+par(medidasProfilaticas,`Vacinacao`).
+par(medidasProfilaticas,`Evitar Contato com Doentes`).
+par(medidasProfilaticas,`Erradicacao dos Insetos Vetores`).
+par(medidasProfilaticas,`Vacinacao de Animais`).
