d0f01dc037320835379a8a377866d473bc4861ca
Rudmar <rudmar_junior@hotmail.com>
2014-05-18 21:08:37 -0300

    Atualizacao da Listas

--- listas.pl
+++ listas.pl
@@ -6,12 +6,27 @@ par(doenca,`Raiva`).
 par(doenca,`Gripe`).
 par(doenca,`Catapora`).
 
+par(doenca,`Aspergilose`).
+par(doenca,`Candidiase`).
+par(doenca,`Criptococose`).
+par(doenca,`Dermatite Seborreica`).
+par(doenca,`Dermatofitose`).
+
 par(formaTransmissao,`Saliva`).
 par(formaTransmissao,`Contato com Objeto Contaminado`).
 par(formaTransmissao,`Picada`).
 par(formaTransmissao,`Mordedura de Animal Infectado`).
 
+par(formaTransmissao,`Alimento`).
+par(formaTransmissao,`Agua`).
+par(formaTransmissao,`Relacao Sexual Desprotegida`).
+par(formaTransmissao,`Inalacao dos Esporos`).
+par(formaTransmissao,`Desconhecida`).
+par(formaTransmissao,`Contato Direto com Local Contaminado`).
+
 par(agenteCausador,`Virus`).
+
+par(agenteCausador,`Fungos`).
 
 par(sintoma,`Inflamacao das Parotidas`).
 par(sintoma,`Aspecto Amarelado da Pele`).
@@ -24,7 +39,32 @@ par(sintoma,`Tosse`).
 par(sintoma,`Dores Musculares`).
 par(sintoma,`Numerosas Feridas pelo Corpo`).
 
+par(sintoma,`Vomito`).
+par(sintoma,`Diarreia`).
+par(sintoma,`Nausea`).
+par(sintoma,`Dor na Boca`).
+par(sintoma,`Vermelhidao na Boca`).
+par(sintoma,`Vermelhidao na Regiao Genital`).
+par(sintoma,`Dor de Cabeca`).
+par(sintoma,`Fotofobia`).
+par(sintoma,`Vermelhidao da Pele`).
+par(sintoma,`Oleosidade da Pele`).
+par(sintoma,`Inflamacao da Pele`).
+par(sintoma,`Coceira`).
+par(sintoma,`Vermelhidao Genital em Forma Circular`).
+
 par(medidasProfilaticas,`Vacinacao`).
 par(medidasProfilaticas,`Evitar Contato com Doentes`).
 par(medidasProfilaticas,`Erradicacao dos Insetos Vetores`).
 par(medidasProfilaticas,`Vacinacao de Animais`).
+
+par(medidasProfilaticas,`Lavar Alimento`).
+par(medidasProfilaticas,`Ferver Agua`).
+par(medidasProfilaticas,`Evitar Sexo Desprotegido`).
+par(medidasProfilaticas,`Evitar Beijo em Pessoas Desconhecidas`).
+par(medidasProfilaticas,`Evitar Contato Direto com Detrito de Pombo`).
+par(medidasProfilaticas,`Evitar Estresse`).
+par(medidasProfilaticas,`Lavar Adaquadamente o Local`).
+par(medidasProfilaticas,`Evitar Agua Excessivamente Quente`).
+par(medidasProfilaticas,`Qualidade do Sono`).
+par(medidasProfilaticas,`Evitar Umidade na Pele`).
